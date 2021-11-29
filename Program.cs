fab45e23ad041312436d35efe1cfe76e76142c94
Biglanchik <95071859+Biglanchik@users.noreply.github.com>
2021-11-29 17:03:01 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace ReposVanekDen
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello House!");
         }
     }
 }
