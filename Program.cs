37e859722b21039baf12f3d4f588324119bdd1d1
enoenot999@yandex.ru <enoenot999@yandex.ru>
2021-11-29 16:58:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReposVanekDen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
